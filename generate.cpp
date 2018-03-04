3a39fc19c5f1e1448fc8659a7012197e015dc725
Takeshi Ikuma <tikuma@gmail.com>
2018-03-04 15:01:25 -0600

    under construction

--- generate.cpp
+++ generate.cpp
@@ -1,3 +1,21 @@
+#include <fstream>
+
+using namespace std;
+
+void main()
+{
+    string outfile = "C:/Users/tikum/Documents/GitHub/matlab-nlopt/build/+nlopt/@options/nlopt_algorigthm_idstr.h";
+    string infile = "C:/Users/tikum/Documents/GitHub/matlab-nlopt/build/nlopt/include/nlopt.h";
+
+    ofstream out(outfile);
+    out << "#pragma once" << endl
+    <<"#include ""nlopt.h""" << endl
+    << endl
+    <<"static const char nlopt_algorithm_idstrs[NLOPT_NUM_ALGORITHMS][256] = {" << endl;
+
+    std::ifstream in(infile);
+
+}
 // static const char nlopt_algorithm_short_names[NLOPT_NUM_ALGORITHMS][256] = {
 
 // nlopt.h
