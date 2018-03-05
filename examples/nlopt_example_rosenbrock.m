edaaabdbb19497ffe511c60bd53bc3c7369c99fa
Takeshi Ikuma <tikuma@lsu.edu>
2018-03-05 12:43:31 -0600

    added gradient-based search

--- examples/nlopt_example_rosenbrock.m
+++ examples/nlopt_example_rosenbrock.m
@@ -15,9 +15,26 @@ h = line(x0(1),x0(2),'Marker','.','Color','k');
 options = nlopt.options('ln_neldermead',2,'OutputFun',@(x,optimvalues,state)outfun(x,optimvalues,state,h));
 [x, fval, flag, output] = nlopt.fminunc(anonrosen,x0,options);
 
+h1 = line(x0(1),x0(2),'Marker','.','Color','r','LineStyle','--');
+options = nlopt.options('ld_lbfgs',2,'OutputFun',@(x,optimvalues,state)outfun(x,optimvalues,state,h1));
+[x, fval, flag, output] = nlopt.fminunc(@rosenbrockwithgrad,x0,options);
+
 line(x(1),x(2),'Marker','o','LineStyle','none','Color','w','MarkerFaceColor','w')
 
-function stop = outfun(x,optimvalues,state,h)
+legend([h h1],'Nelder-Mead', 'Limited-memory BFGS')
+
+
+function stop = outfun(x,~,~,h)
   set(h,'XData',[h.XData x(1)],'YData',[h.YData x(2)]);
   stop = false;
+end
+
+function [f,g] = rosenbrockwithgrad(x)
+% Calculate objective f
+f = 100*(x(2,:) - x(1,:).^2).^2 + (1-x(1,:)).^2;
+
+if nargout > 1 % gradient required
+    g = [-400*(x(2,:)-x(1,:).^2).*x(1,:)-2*(1-x(1,:));
+        200*(x(2,:)-x(1,:).^2)];
+end
 end
